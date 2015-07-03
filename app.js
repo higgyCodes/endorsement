ea2d935b5ce1d7d1c408894cada2f63eb0192cd1
pathiggins13 <patrickjameshiggins88@gmail.com>
2015-07-03 12:27:33 -0600

    Json additions

--- app.js
+++ app.js
@@ -1,28 +1,68 @@
 
-var utahVotes = angular.module('utahVotes', ['ngRoute']);
+var utahVotes = angular.module('utahVotes', ['ui.router']);
 
-utahVotes.config(function($routeProvider) {
-    $routeProvider
-        .when('/endorsements', {
-            templateUrl: 'endorsements.html',
-            controller: 'myCtrl'
+utahVotes.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home')
+        
+    $stateProvider
+    
+    // Home state
+
+        .state('home', {
+            url: '/home',
+            templateUrl: 'home.html'
         })
-        .when('/lobbying', {
-            templateUrl: 'lobbying.html',
-            controller: 'myCtrl'
+
+
+    // Endorsments and nested views
+
+        .state('endorsements', {
+            url: '/endorsements',
+            templateUrl: 'endorsements.html'
         })
-                .when('/other', {
-            templateUrl: 'other.html',
-            controller: 'CalcCtrl'
+
+        .state('endorsements.racetype', {
+            url: '/racetype',
+            templateUrl: 'racetype.html'
         })
-        .otherwise({
-            redirectTo: '/endorsements.html'
-        });
+
+         .state('endorsements.racetype.menu', {
+            url: '/menu',
+            templateUrl: 'menu.html'
+            
+        })
+
+         .state('endorsements.racetype.municipal', {
+            url: "/municipal ",
+            templateUrl: 'municipal.html'
+            
+        })
+         .state('endorsements.racetype.city', {
+            url: "/city",
+            templateUrl: 'city.html'
+            
+        })
+         .state('endorsements.racetype.race', {
+            url: "/race",
+            templateUrl: 'race.html'
+            
+        })
+
+
+    // Lobbying and nested views
+
+        .state('lobbying', {
+            url: '/lobbying',
+            templateUrl: 'lobbying.html'
+        })
+
+        // Nested Endorsements States
+       
 });
 
 utahVotes.controller('myCtrl', ['$scope', function($scope){
 
-	$scope.filterBy = '';
+	$scope.cities = ['Salt Lake', 'South Salt Lake', 'Draper', 'Midvale']
 
 	$scope.setFilter = function(whichFilter){
 		if ($scope.filterBy === whichFilter) {
@@ -31,64 +71,5 @@ utahVotes.controller('myCtrl', ['$scope', function($scope){
 			$scope.filterBy = whichFilter;
 		}
 	};
-
-	$scope.transactions = [
-	{
-		title: 'a',
-		cost: 6,
-		notes: 'x'
-	},
-	{
-		title: 'b',
-		cost: 1,
-		notes: 'w'
-	},
-	{
-		title: 'c',
-		cost: 2,
-		notes: 'v'
-	},
-	{
-		title: 'd',
-		cost: 3,
-		notes: 'u'
-	},
-	{
-		title: 'e',
-		cost: 4,
-		notes: 'z'
-	},
-	{
-		title: 'f',
-		cost: 5,
-		notes: 'y'
-	}
-	];
-
-	$scope.sum = function() {
-		$scope.total = "";
-		for (var i = 0; i < $scope.transactions.length; i++) {
-			$scope.total += "+" + $scope.transactions[i].cost;
-		}
-		$scope.total = eval($scope.total);
-		return $scope.total;
-	};
-
-	 $scope.submit = function() {
-          var newItem = {};
-          newItem.title = $scope.title;
-          newItem.cost = $scope.cost;
-          newItem.notes = $scope.notes;
-          newItem.createdOn = Date.now();
-
-          $scope.transactions.push(newItem);
-
-
-          $scope.title	= '';
-          $scope.cost	= '';
-          $scope.notes	= '';
-        };
-
-
 
 }]);
