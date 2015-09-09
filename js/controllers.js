cc576f469f1afa41a1e08c69dd274dbd76bc8f2e
pathiggins13 <pat_higgins@hotmail.com>
2015-09-09 15:11:18 -0600

    figured the county list

--- js/controllers.js
+++ js/controllers.js
@@ -12,23 +12,19 @@ utahVotes.controller('myCtrl', ['$scope', function($scope){
 
 
 utahVotes.controller("fireCtrl", function($scope, $firebaseObject) {
-  $scope.select = "Hi Guys"
-  $scope.test = "This Thing"
   var ref = new Firebase("https://utah-votes.firebaseio.com/");
   var obj = $firebaseObject(ref);
   var years = obj.years;
   obj.$loaded().then(function() {
     var typeList = [];
+    var countyList = [];
+    
     for (i = 0; i < obj.entries.length; i++) {
-      console.log(obj.entries[i])
       var counter = 0;
       for (s = 0; s < typeList.length; s++) {
-        
         var type = typeList[s];
-        console.log(type)
         if (obj.entries[i].race === type) {
           counter += 1
-          console.log(counter)
         }
       } 
       console.log(counter)
@@ -42,7 +38,19 @@ utahVotes.controller("fireCtrl", function($scope, $firebaseObject) {
 
   $scope.navigate = function(item) {
     for (i = 0; i < obj.entries.length; i++) {
-      obj.entries[i].
+      var counter = 0
+      for (s = 0; s < countyList.length; s++) {
+        county = countyList[s];
+        
+        if (obj.entries[i].race === type) {
+          counter += 1
+        }
+      }
+    console.log(counter)
+      if (counter == 0) {
+          countyList.push(obj.entries[i].county)
+          console.log(countyList);
+      }
     }
   }
 
