e841c9c0e06bd51552ba2f4b48f7eae6a51151e0
pathiggins13 <pat_higgins@hotmail.com>
2015-09-09 12:37:58 -0600

    creates click function with new backend

--- js/controllers.js
+++ js/controllers.js
@@ -12,42 +12,39 @@ utahVotes.controller('myCtrl', ['$scope', function($scope){
 
 
 utahVotes.controller("fireCtrl", function($scope, $firebaseObject) {
+  $scope.select = "Hi Guys"
+  $scope.test = "This Thing"
   var ref = new Firebase("https://utah-votes.firebaseio.com/");
-
   var obj = $firebaseObject(ref);
   var years = obj.years;
   obj.$loaded().then(function() {
-    console.log("loaded record:", obj)
-    
-    $scope.selected = obj.years
-    console.log(obj.years)
+    var typeList = [];
+    for (i = 0; i < obj.entries.length; i++) {
+      console.log(obj.entries[i])
+      var counter = 0;
+      for (s = 0; s < typeList.length; s++) {
+        
+        var type = typeList[s];
+        console.log(type)
+        if (obj.entries[i].race === type) {
+          counter += 1
+          console.log(counter)
+        }
+      } 
+      console.log(counter)
+      if (counter == 0) {
+        typeList.push(obj.entries[i].race)
+        console.log(typeList);
+      }
+    }
 
-    var breadcrumb = []
-    var placehold = []
-    var ladder = []
+  $scope.select = typeList;
 
-    $scope.select = function(data) {
-      if (breadcrumb[3] === true) {
-        breadcrumb[4] = data
-      } else if (breadcrumb[2] === true) {
-        breadcrumb[3] = data
-      } else if (breadcrumb[1] === true) {
-        breadcrumb[2] = data;
-      } else if (ladder[0] === true) {
-        breadcrumb[1] = data
-        $scope.selected = obj.counties
-        console.log($scope.selected)
-      } else {
-        ladder[0] = true
-        breadcrumb[0] = data;
-        console.log('last statement')
-        placehold = breadcrumb[0]
-        $scope.selected = obj.year[data].electiontype;
-        console.log(data)
-      };
-    };
-
-
+  $scope.navigate = function(item) {
+    for (i = 0; i < obj.entries.length; i++) {
+      obj.entries[i].
+    }
+  }
 
   });
 
